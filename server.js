2ab43a2ffc338230089010b992162d9475493b55
lestra00 <74250508+lestra00@users.noreply.github.com>
2024-08-19 13:42:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/power-tables-v0.2'));
+app.use(express.static(__dirname + '/dist/power-tables/browser'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/POWER_TABLES/index.html'));
+res.sendFile(path.join(__dirname+'/dist/power-tables/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
